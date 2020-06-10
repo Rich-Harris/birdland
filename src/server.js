455425239937606c28f3e43fcc83de41e3e5646a
Richard Harris <richard.a.harris@gmail.com>
2020-06-09 23:43:58 -0400

    add vercel-sapper

--- src/server.js
+++ src/server.js
@@ -7,12 +7,16 @@ import * as sapper from '@sapper/server';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-express() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+const app = express();
+
+app.use(
+	compression({ threshold: 0 }),
+	sirv('static', { dev }),
+	sapper.middleware()
+);
+
+app.listen(PORT, err => {
+	if (err) console.log('error', err);
+});
+
+export default app;
