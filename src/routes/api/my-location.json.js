4537235243ba59ccde2b34ebccab5d2404907c78
Richard Harris <richard.a.harris@gmail.com>
2020-06-09 23:53:44 -0400

    use x-forwarded-for

--- src/routes/api/my-location.json.js
+++ src/routes/api/my-location.json.js
@@ -1,9 +1,7 @@
 import * as httpie from 'httpie';
 
 export async function get(req, res) {
-	const ip = req.ip === '::1' || req.ip === '127.0.0.1'
-		? ''
-		: req.ip;
+	const ip = req.headers['x-forwarded-for'] || '';
 
 	const { data } = await httpie.get(`https://freegeoip.app/json/${ip}`);
 	res.send(data);
