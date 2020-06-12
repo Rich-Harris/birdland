dca473fbd16960e176140c62b135893ba8368bc3
Antony Jones <antony@beyonk.com>
2020-06-12 16:51:19 +0100

    missing file

---
+++ src/routes/cities/[country]/[state]/[name]/_components/Attribution.svelte
@@ -0,0 +1,10 @@
+<div class="attribution">	
+  <p>Weather data provided by <a href="https://www.weatherbit.io/">Weatherbit.io</a>, used unmodifed under <a href="https://creativecommons.org/licenses/by/2.0/">CC BY</a>.</p>
+</div>
+
+<style>
+	.attribution {
+		margin: 8px;
+		font-size: smaller;
+	}
+</style>
